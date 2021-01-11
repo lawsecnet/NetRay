eda1d4ac7f293d4c8914d26f8a0c1eb790c8ab74
Kamil <kamilbojarski@protonmail.com>
2021-01-11 16:06:03 +0100

    Shodan, PassiveTotal PDNS modules added, UI fixes.

--- netray.py
+++ netray.py
@@ -4,6 +4,7 @@ import re
 import pprint as pp
 import socket
 import requests
+from shodan import Shodan
 
 mainWindow = tk.Tk()
 mainWindow.title("NetRay")
@@ -16,9 +17,11 @@ frm_buttons = tk.Frame(mainWindow)
 lbl_domainInput = tk.Label(master=frm_basic, text="Domain/IP: ")
 lbl_passiveTotalApi = tk.Label(master=frm_basic, text="Passive Total api key:")
 lbl_passiveTotalEmail = tk.Label(master=frm_basic, text="Passive Total email:")
+lbl_shodanApi = tk.Label(master=frm_basic, text="Shodan API key:")
 ent_domainInput = tk.Entry(master=frm_basic,width=100)
 ent_passiveTotalEmail = tk.Entry(master=frm_basic, width=100)
 ent_passiveTotalApi = tk.Entry(master=frm_basic, width=100)
+ent_shodanApi = tk.Entry(master=frm_basic, width=100)
 lbl_result = tk.Label(master=mainWindow, text="Scan results")
 
 def scan_domain():
@@ -58,10 +61,16 @@ def passivetotal_lookup():
     pdnsresults_format = pp.pformat(pdns_results)
     lbl_result["text"] = pdnsresults_format
 
-def ping_domain():
+def shodan_lookup():
+    key = ent_shodanApi.get()
+    shodan_api = Shodan(key)    
     target_domain = str(ent_domainInput.get())
-    print(target_domain)
+    target = socket.gethostbyname(target_domain)
 
+    shodan_info = shodan_api.host(target)
+    shodan_results = pp.pformat(shodan_info)
+    lbl_result["text"] = shodan_results
+    
 btn_scanButton = tk.Button(
     frm_buttons,
     text="Scan WHOIS",
@@ -69,12 +78,12 @@ btn_scanButton = tk.Button(
     height = 2,
     command=scan_domain)
 
-btn_pingButton = tk.Button(
+btn_shodanButton = tk.Button(
     frm_buttons,
-    text = "Ping host",
+    text = "Shodan lookup",
     width=20,
     height=2,
-    command=ping_domain)
+    command=shodan_lookup)
 
 btn_passiveTotalLookup = tk.Button(
     frm_buttons,
@@ -86,14 +95,16 @@ btn_passiveTotalLookup = tk.Button(
 
 frm_basic.grid(row=0, column=0, padx=10)
 frm_buttons.grid(row=0, column=3, sticky="ne")
-lbl_domainInput.grid(row=0, column=0,)
-ent_domainInput.grid(row=0, column=1,)
-lbl_passiveTotalEmail.grid(row=1, column=0)
+lbl_domainInput.grid(row=0, column=0, sticky="w")
+ent_domainInput.grid(row=0, column=1)
+lbl_passiveTotalEmail.grid(row=1, column=0,sticky="w")
 ent_passiveTotalEmail.grid(row=1, column=1)
-lbl_passiveTotalApi.grid(row=2, column=0)
+lbl_passiveTotalApi.grid(row=2, column=0, sticky="w")
 ent_passiveTotalApi.grid(row=2, column=1)
+lbl_shodanApi.grid(row=3, column=0, sticky="w")
+ent_shodanApi.grid(row=3, column=1)
 btn_scanButton.grid(row=0, column=2)
-btn_pingButton.grid(row=1, column=2)
+btn_shodanButton.grid(row=1, column=2)
 btn_passiveTotalLookup.grid(row=2, column=2)
 lbl_result.grid(row=1, column=0, padx=10, pady=10, sticky="w")
 

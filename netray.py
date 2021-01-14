d1c2674f23ecfd4d70c45d0ba6b796cd84170b84
Kamil <kamilbojarski@protonmail.com>
2021-01-14 21:41:44 +0100

    Fixed UI to show results in scrollable text field.
    Enables proper display of longer results.

--- netray.py
+++ netray.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter.scrolledtext import ScrolledText
 from ipwhois import IPWhois
 import re
 import pprint as pp
@@ -8,7 +9,7 @@ from shodan import Shodan
 
 mainWindow = tk.Tk()
 mainWindow.title("NetRay")
-mainWindow.resizable(width=True, height=True)
+mainWindow.resizable(width=False, height=False)
 
 
 frm_basic = tk.Frame(master=mainWindow)
@@ -22,9 +23,10 @@ ent_domainInput = tk.Entry(master=frm_basic,width=100)
 ent_passiveTotalEmail = tk.Entry(master=frm_basic, width=100)
 ent_passiveTotalApi = tk.Entry(master=frm_basic, width=100)
 ent_shodanApi = tk.Entry(master=frm_basic, width=100)
-lbl_result = tk.Label(master=mainWindow, text="Scan results")
+lbl_result = ScrolledText(master=mainWindow, width=117)
 
-def scan_domain():
+
+def scan_whois():
     target_domain = str(ent_domainInput.get())
 
     if re.search(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", target_domain) is not None:
@@ -41,7 +43,11 @@ def scan_domain():
 
     presults = pp.pformat(whoisResults, indent=2)
 
-    lbl_result["text"] = presults
+    lbl_result.configure(state="normal")
+    lbl_result.delete("1.0", tk.END)
+    lbl_result.insert("1.0", presults)
+    lbl_result.configure(state="disabled")
+    lbl_result.bind("<Button>", lambda event: lbl_result.focus_set())
 
 def passivetotal_lookup():
     username = ent_passiveTotalEmail.get()
@@ -57,9 +63,13 @@ def passivetotal_lookup():
         return response.json()
 
     pdns_results = passivetotal_get('/v2/dns/passive', str(ent_domainInput.get()))
+    pdns_formated = pp.pformat(pdns_results)
 
-    pdnsresults_format = pp.pformat(pdns_results)
-    lbl_result["text"] = pdnsresults_format
+    lbl_result.configure(state="normal")
+    lbl_result.delete("1.0", tk.END)
+    lbl_result.insert("1.0", pdns_formated)
+    lbl_result.configure(state="disabled")
+    lbl_result.bind("<Button>", lambda event: lbl_result.focus_set())
 
 def shodan_lookup():
     key = ent_shodanApi.get()
@@ -69,14 +79,20 @@ def shodan_lookup():
 
     shodan_info = shodan_api.host(target)
     shodan_results = pp.pformat(shodan_info)
-    lbl_result["text"] = shodan_results
+
+    lbl_result.configure(state="normal")
+    lbl_result.delete("1.0", tk.END)
+    lbl_result.insert("1.0", shodan_results)
+    lbl_result.configure(state="disabled")
+    lbl_result.bind("<Button>", lambda event: lbl_result.focus_set())
     
-btn_scanButton = tk.Button(
+
+btn_whoisButton = tk.Button(
     frm_buttons,
     text="Scan WHOIS",
     width = 20,
     height = 2,
-    command=scan_domain)
+    command=scan_whois)
 
 btn_shodanButton = tk.Button(
     frm_buttons,
@@ -90,8 +106,7 @@ btn_passiveTotalLookup = tk.Button(
     text= "PT PDNS lookup",
     width=20,
     height=2,
-    command=passivetotal_lookup
-)
+    command=passivetotal_lookup)
 
 frm_basic.grid(row=0, column=0, padx=10)
 frm_buttons.grid(row=0, column=3, sticky="ne")
@@ -103,7 +118,7 @@ lbl_passiveTotalApi.grid(row=2, column=0, sticky="w")
 ent_passiveTotalApi.grid(row=2, column=1)
 lbl_shodanApi.grid(row=3, column=0, sticky="w")
 ent_shodanApi.grid(row=3, column=1)
-btn_scanButton.grid(row=0, column=2)
+btn_whoisButton.grid(row=0, column=2)
 btn_shodanButton.grid(row=1, column=2)
 btn_passiveTotalLookup.grid(row=2, column=2)
 lbl_result.grid(row=1, column=0, padx=10, pady=10, sticky="w")

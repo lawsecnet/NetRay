78b36b49f1c415f810e66486f59427f7ab019325
lawsecnet <kamil.bojarski@lawsec.net>
2023-07-06 09:12:25 +0200

    Censys certificate search added

--- netray.py
+++ netray.py
@@ -5,7 +5,10 @@ import re
 import pprint as pp
 import socket
 import requests
+import os
+import json
 from shodan import Shodan
+from censys.search import CensysCerts
 
 mainWindow = tk.Tk()
 mainWindow.title("NetRay")
@@ -15,14 +18,18 @@ mainWindow.resizable(width=False, height=False)
 frm_basic = tk.Frame(master=mainWindow)
 frm_buttons = tk.Frame(mainWindow)
 
-lbl_domainInput = tk.Label(master=frm_basic, text="Domain/IP: ")
+lbl_domainInput = tk.Label(master=frm_basic, text="Domain/IP/Certificate: ")
 lbl_passiveTotalApi = tk.Label(master=frm_basic, text="Passive Total api key:")
 lbl_passiveTotalEmail = tk.Label(master=frm_basic, text="Passive Total email:")
 lbl_shodanApi = tk.Label(master=frm_basic, text="Shodan API key:")
+lbl_censysAPIID = tk.Label(master=frm_basic, text="Censys API ID")
+lbl_censysAPIS = tk.Label(master=frm_basic, text="Censys API Secret")
 ent_domainInput = tk.Entry(master=frm_basic,width=100)
 ent_passiveTotalEmail = tk.Entry(master=frm_basic, width=100)
 ent_passiveTotalApi = tk.Entry(master=frm_basic, width=100)
 ent_shodanApi = tk.Entry(master=frm_basic, width=100)
+ent_censysAPIID = tk.Entry(master=frm_basic, width=100)
+ent_censysAPIS = tk.Entry(master=frm_basic, width=100)
 lbl_result = ScrolledText(master=mainWindow, width=117)
 
 
@@ -85,6 +92,24 @@ def shodan_lookup():
     lbl_result.insert("1.0", shodan_results)
     lbl_result.configure(state="disabled")
     lbl_result.bind("<Button>", lambda event: lbl_result.focus_set())
+
+def censys_cert_search():
+    apiid = str(ent_censysAPIID.get())
+    apis = str(ent_censysAPIS.get())
+    cert = str(ent_domainInput.get())
+
+    os.environ["CENSYS_API_ID"] = apiid
+    os.environ["CENSYS_API_SECRET"] = apis
+    cen = CensysCerts()
+    
+    censys_results = cen.view(cert)
+    results_p = json.dumps(censys_results, indent=2)
+
+    lbl_result.configure(state="normal")
+    lbl_result.delete("1.0", tk.END)
+    lbl_result.insert("1.0", results_p)
+    lbl_result.configure(state="disabled")
+    lbl_result.bind("<Button>", lambda event: lbl_result.focus_set())
     
 
 btn_whoisButton = tk.Button(
@@ -108,6 +133,14 @@ btn_passiveTotalLookup = tk.Button(
     height=2,
     command=passivetotal_lookup)
 
+btn_censysLookup = tk.Button(
+     frm_buttons,
+     text="Censys Cert Search",
+     width=20,
+     height=2,
+     command=censys_cert_search
+)
+
 frm_basic.grid(row=0, column=0, padx=10)
 frm_buttons.grid(row=0, column=3, sticky="ne")
 lbl_domainInput.grid(row=0, column=0, sticky="w")
@@ -118,9 +151,14 @@ lbl_passiveTotalApi.grid(row=2, column=0, sticky="w")
 ent_passiveTotalApi.grid(row=2, column=1)
 lbl_shodanApi.grid(row=3, column=0, sticky="w")
 ent_shodanApi.grid(row=3, column=1)
+lbl_censysAPIID.grid(row=4, column=0, sticky="w")
+ent_censysAPIID.grid(row=4, column=1)
+lbl_censysAPIS.grid(row=5, column=0, sticky="w")
+ent_censysAPIS.grid(row=5, column=1)
 btn_whoisButton.grid(row=0, column=2)
 btn_shodanButton.grid(row=1, column=2)
 btn_passiveTotalLookup.grid(row=2, column=2)
+btn_censysLookup.grid(row=3, column=2)
 lbl_result.grid(row=1, column=0, padx=10, pady=10, sticky="w")
 
 mainWindow.mainloop()
